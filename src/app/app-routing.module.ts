655b7370e0b87572bf9b99728e9ec1baa937d7fb
Miłosz Gilga <gilgamilosz451@gmail.com>
2023-05-14 17:25:19 +0200

    init commit (scaffold basic configuration)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+/*
+ * Copyright (c) 2023 by MILOSZ GILGA <http://miloszgilga.pl>
+ *
+ * File name: app-routing.module.ts
+ * Last modified: 14/05/2023, 17:07
+ * Project name: stars-magnet-client
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL COPIES OR
+ * SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ *
+ * The software is provided "as is", without warranty of any kind, express or implied, including but not limited
+ * to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no event
+ * shall the authors or copyright holders be liable for any claim, damages or other liability, whether in an
+ * action of contract, tort or otherwise, arising from, out of or in connection with the software or the use
+ * or other dealings in the software.
+ */
+
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const routes: Routes = [];
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes),
+    ],
+    exports: [
+        RouterModule,
+    ],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,61 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+/*
+ * Copyright (c) 2023 by MILOSZ GILGA <http://miloszgilga.pl>
+ *
+ * File name: app.component.spec.ts
+ * Last modified: 14/05/2023, 17:07
+ * Project name: stars-magnet-client
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL COPIES OR
+ * SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ *
+ * The software is provided "as is", without warranty of any kind, express or implied, including but not limited
+ * to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no event
+ * shall the authors or copyright holders be liable for any claim, damages or other liability, whether in an
+ * action of contract, tort or otherwise, arising from, out of or in connection with the software or the use
+ * or other dealings in the software.
+ */
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+import { TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { AppComponent } from "./app.component";
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  it(`should have as title 'stars-magnet-client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('stars-magnet-client');
-  });
+describe("AppComponent", () => {
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule
+            ],
+            declarations: [
+                AppComponent
+            ],
+        }).compileComponents();
+    });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('stars-magnet-client app is running!');
-  });
+    it("should create the app", () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
+    });
+
+    it(`should have as title "Hello from angular!"`, () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app.title).toEqual("Hello from angular!");
+    });
+
+    it("should render title", () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('.content span')?.textContent).toContain("stars-magnet-client app is running!");
+    });
 });
